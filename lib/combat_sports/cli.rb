4a9a8f696c210e3aa4c640d3c7b77c1820afa755
jacobloris <jacobloris@outlook.com>
2022-01-24 18:22:44 -0700

    Started cli.rb, gym.rb, and scraper.rb all simultaneously so I can build off of each other.

--- lib/combat_sports/cli.rb
+++ lib/combat_sports/cli.rb
@@ -1,7 +1,7 @@
 class CombatSports::CLI
 
     def run
-        CombatSports::Scraper.scrape 
+    #    CombatSports::Scraper.scrape 
         prompt_user_input
     end
 
@@ -11,8 +11,8 @@ class CombatSports::CLI
         puts "BEST RATED MMA GYMS IN THE WORLD!\n"\
             "Type in 'location' to get a list of where they are all located.\n"\
             "Type in 'specialty' to get a list of what each gym specializes in (Muay Thai, BJJ, etc.).\n"\
-            "Type in 'coach' to get a list of the coaches at each gym.\n"\n 
+            "Type in 'coach' to get a list of the coaches at each gym.\n"\
             "Type in 'fighters' to see which famous fighters come from what gym.\n"\
             "Type in 'exit!' to quit."
     end
-    
+end
