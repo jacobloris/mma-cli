eea2555ef61f9b4881af96a44f7dd956eb3b021d
jacobloris <jacobloris@outlook.com>
2022-01-28 16:03:57 -0700

    Finished scraper.rb

--- lib/combat_sports/cli.rb
+++ lib/combat_sports/cli.rb
@@ -9,8 +9,8 @@ class CombatSports::CLI
 
     def prompt_user_input
         puts "BEST RATED MMA GYMS IN THE WORLD!\n"\
-            "Type in 'location' to get a list of where they are all located.\n"\
-            "Type in 'specialty' to get a list of what each gym specializes in (Muay Thai, BJJ, etc.).\n"\
+            "Type in 'name' to get a list of each gym.\n"\
+            "Type in 'description to get a brief description of each gym.\n"\
             "Type in 'coach' to get a list of the coaches at each gym.\n"\
             "Type in 'fighters' to see which famous fighters come from what gym.\n"\
             "Type in 'exit!' to quit."
--- lib/combat_sports/scraper.rb
+++ lib/combat_sports/scraper.rb
@@ -9,21 +9,21 @@ class Scraper
     def get_page 
         doc = Nokogiri::HTML(open("https://martialarts-hq.com/the-top-11-mma-gyms-in-the-world"))
         
-        binding.pry
+       binding.pry
     end
    
 
-    #def get_gyms
-     #   self.get_page(".div")
-    #end
+    def get_gyms
+        self.get_page(".div")
+    end
 
-   # def make_gym
-        #self.get_gyms.each do |div|
-         #   gym = Gym.new
-          #  gym.title = doc.css("h3").text
-           # gym.description = doc.css("span").text
-        #end
-    #end
+    def make_gym
+        self.get_gyms.each do |div|
+            gym = Gym.new
+            gym.title = doc.css("h3").text
+            gym.description = doc.css("span").text
+        end
+    end
 end
 
 
